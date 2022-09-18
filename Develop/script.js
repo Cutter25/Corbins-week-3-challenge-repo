90c75398e8f005fa4fa60f4bf3878e6c001e11ae
Corbin Cutter <Corbin@Lisas-iMac.local>
2022-09-17 20:49:14 -0400

    Working on prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,41 @@
 // Assignment code here
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+   //var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+   var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   passwordText.value = password;
+
+ // return chars[Math.floor(Math.random() * chars.length)];
 
 }
 
+// console.log(writePassword());
+
+function prompt() {
+  var upperCase = prompt("Include an upper case letter in password?");
+
+  if (upperCase != null) {
+    console.log("test")
+  }
+
+
+
+
+  let lowerCase = prompt("Include an lower case letter in password?");
+  let number = prompt("Include a number in password?");
+  let specialCharacter = prompt("Include a special character in password?");
+
+ }
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", prompt());
