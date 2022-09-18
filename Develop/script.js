48bb13b3bb73ebb660fc03ec62a03977ed95be5f
Corbin Cutter <Corbin@Lisas-iMac.local>
2022-09-17 22:37:56 -0400

    Time to Debug

--- Develop/script.js
+++ Develop/script.js
@@ -12,42 +12,56 @@ var numberCharacters = ['1','2','3','4','5','6','7','8','9','0',];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+
 // Write password to the #password input
 function writePassword() {
-   //var password = generatePassword();
-   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  var correctPrompts = prompts(); 
+  var passwordText = document.querySelector("#password");
+
+  if(correctPrompts){
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
 
 }
 
 function generatePassword() {
-
-
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * paramterCharacters.length);
+    password = password + paramterCharacters[randomIndex];
+  }
+  return password;
 }
+// Window Prompts below 
 
 function prompts() {
+  paramterCharacters = [];
+
   pswrdLength = parseInt(prompt("Bewtween 8 and 128, how many characters would you like your password to be?"));
 
-  if(isNaN(pswrdLength) || pswrdLength < 8 || pswrdLength > 128)
+  if(isNaN(pswrdLength) || pswrdLength < 8 || pswrdLength > 128) {
     alert("Character length must be in between 8 and 128 characters. Try again.");
     return false;
-}
-
-if (confirm("Would you like to include lower case letters in your password?")) {
+  }
+  if (confirm("Would you like to include special characters in your password?")) {
   paramterCharacters = paramterCharacters.concat(specialCharacters);
-}
+  }
 
-if (confirm("Would you like to include lower case letters in your password?")) {
+  if (confirm("Would you like to include lower case letters in your password?")) {
   paramterCharacters = paramterCharacters.concat(lowerCaseCharacters);
-}
+  }
 
-if (confirm("Would you like to include upper case letters in your password?")) {
+  if (confirm("Would you like to include upper case letters in your password?")) {
   paramterCharacters = paramterCharacters.concat(upperCaseCharacters);
-}
+  }
 
-if (confirm("Would you like to include numbers in your password?")) {
+  if (confirm("Would you like to include numbers in your password?")) {
   paramterCharacters = paramterCharacters.concat(numberCharacters);
+  }
+  return true;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", prompt());
